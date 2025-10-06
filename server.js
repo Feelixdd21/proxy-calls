f159a605df5d1825a18869e0f31814665f197424
Feelixdd21 <110635197+Feelixdd21@users.noreply.github.com>
2025-10-06 15:29:37 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.post("/incoming_call", (req, res) => {
   const twiml = new twilio.twiml.VoiceResponse();
 
   // Redirige la llamada al número real
-  twiml.dial("+522741155287"); 
+ twiml.dial({ callerId: "+19342482237" }, "+522741156331");
 
   res.type("text/xml");
   res.send(twiml.toString());
